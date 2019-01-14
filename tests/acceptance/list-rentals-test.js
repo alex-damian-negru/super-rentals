30f33fb5250ad4799001c470a1378673a18bf909
Alex Negru <alex.negru@agilefreaks.com>
2019-01-14 18:00:20 +0200

    Update navigation tests

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,24 +1,37 @@
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
+import {
+  click,
+  currentURL,
+  visit
+} from '@ember/test-helpers'
 
 module('Acceptance | list rentals', function(hooks) {
   setupApplicationTest(hooks);
 
-  test('should show rentals as the home page', async function(assert) {
+  test('shows rentals as the home page', async function(assert) {
+    await visit('/');
+    assert.equal(currentURL(), '/rentals', 'redirects automatically')
   });
 
-  test('should link to information about the compnay', async function(assert) {
+  test('links to information about the compnay', async function(assert) {
+    await visit('/');
+    await click(".menu-about");
+    assert.equal(currentURL(), '/about', 'navigates to about')
   });
 
-  test('should link to contact information', async function(assert) {
+  test('links to contact information', async function(assert) {
+    await visit('/');
+    await click(".menu-contact");
+    assert.equal(currentURL(), '/contact', 'navigates to contact')
   });
 
-  test('should list available rentals', async function(assert) {
+  test('lists available rentals', async function(assert) {
   });
 
-  test('should filter the list of rentals by city', async function(assert) {
+  test('filters the list of rentals by city', async function(assert) {
   });
 
-  test('should show details for a selected rental', async function(assert) {
+  test('show details for a selected rental', async function(assert) {
   });
 });
