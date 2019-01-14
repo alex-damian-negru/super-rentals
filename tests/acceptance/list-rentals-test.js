45f58ae00cb4c8e463eeb9c27138fc1228ff90c6
Alex Negru <alex.negru@agilefreaks.com>
2019-01-14 16:48:49 +0200

    Add app goals as tests

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,24 @@
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | list rentals', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('should show rentals as the home page', async function(assert) {
+  });
+
+  test('should link to information about the compnay', async function(assert) {
+  });
+
+  test('should link to contact information', async function(assert) {
+  });
+
+  test('should list available rentals', async function(assert) {
+  });
+
+  test('should filter the list of rentals by city', async function(assert) {
+  });
+
+  test('should show details for a selected rental', async function(assert) {
+  });
+});
