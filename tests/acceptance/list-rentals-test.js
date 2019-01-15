b6294e3b3f136f7aafa5789b0d48519ad2b9ab11
Alex Negru <alex.negru@agilefreaks.com>
2019-01-15 14:15:04 +0200

    Install mirage & tutorial-style

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -4,34 +4,46 @@ import {
   click,
   currentURL,
   visit
-} from '@ember/test-helpers'
+} from '@ember/test-helpers';
 
 module('Acceptance | list rentals', function(hooks) {
   setupApplicationTest(hooks);
 
   test('shows rentals as the home page', async function(assert) {
     await visit('/');
-    assert.equal(currentURL(), '/rentals', 'redirects automatically')
+    assert.equal(
+      currentURL(), 
+      '/rentals', 
+      'redirects automatically');
   });
 
   test('links to information about the compnay', async function(assert) {
     await visit('/');
     await click(".menu-about");
-    assert.equal(currentURL(), '/about', 'navigates to about')
+    assert.equal(
+      currentURL(), 
+      '/about', 
+      'navigates to about');
   });
 
   test('links to contact information', async function(assert) {
     await visit('/');
     await click(".menu-contact");
-    assert.equal(currentURL(), '/contact', 'navigates to contact')
+    assert.equal(
+      currentURL(), 
+      '/contact', 
+      'navigates to contact');
   });
 
   test('lists available rentals', async function(assert) {
+    await visit('/');
+    assert.equal(this.element
+      .querySelectorAll('.listing')
+      .length, 3, 
+      'displays 3 listings');
   });
 
-  test('filters the list of rentals by city', async function(assert) {
-  });
+  test('filters the list of rentals by city', async function(assert) {});
 
-  test('show details for a selected rental', async function(assert) {
-  });
+  test('show details for a selected rental', async function(assert) {});
 });
