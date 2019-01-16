71fa2f8d243b1492d1300f22ac9dbd219e4c765e
Alex Negru <alex.negru@agilefreaks.com>
2019-01-16 15:30:07 +0200

    Unit test map service

--- tests/unit/services/map-element-test.js
+++ tests/unit/services/map-element-test.js
@@ -1,12 +1,30 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import { resolve } from 'rsvp';
+
+const DUMMY_ELEMENT = {};
 
 module('Unit | Service | map-element', function(hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
-  test('it exists', function(assert) {
-    let service = this.owner.lookup('service:map-element');
-    assert.ok(service);
-  });
+  test('creates a new map if one isn\'t cached for location', async function(assert) {
+    assert.expect(5);
+
+    let stubMapService = {
+      createMap(element, coords) {
+        assert.ok(element, 'createMap called with element');
+        assert.deepEqual(coords, [0, 0], 'createMap given coordinates');
+        return DUMMY_ELEMENT;
+      }
+    }
+    
+    let stubGeocodeService = {
+      fetchCoordinates(location) {
+        assert.equal(location, 'San Francisco', 'fetchCoordinates called with location');
+        return resolve([0, 0]);
+      }
+    }
+
+    let mapService = this.owner.factoryFor('service:map-element').create({ map: stubMapService, geocode: stubGeocodeService });
+  });  
 });
